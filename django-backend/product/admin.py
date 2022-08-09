e36d2f673fa83dcd8b7a3a48124f62ea78b6ca1c
Mominur Rahman <57674321+mominur774@users.noreply.github.com>
2022-08-09 22:58:44 +0600

    added favorite

--- django-backend/product/admin.py
+++ django-backend/product/admin.py
@@ -1,14 +1,20 @@
 from django.contrib import admin
-from product.models import Product, Cart
+from product.models import Product, Cart, Favorite
 
 # Register your models here.
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price', 'slug', 'short_description', 'full_description',)
+    list_display = ('name', 'price', 'slug',
+                    'short_description', 'full_description',)
 
 
 @admin.register(Cart)
 class CartAdmin(admin.ModelAdmin):
     list_display = ('user', 'product', 'quentity', 'total_price',)
+
+
+@admin.register(Favorite)
+class FavoriteAdmin(admin.ModelAdmin):
+    list_display = ('user', 'product', 'favorite',)
--- django-backend/product/api/v1/urls.py
+++ django-backend/product/api/v1/urls.py
@@ -13,4 +13,8 @@ urlpatterns = [
     path('cart-details/<int:pk>/', views.CartDetails.as_view(), name="cart-details"),
     path('cart-list/', views.CartListView.as_view(), name="cart-list"),
     path('cart-delete/<int:pk>/', views.DeleteCartView.as_view(), name="cart-delete"),
+    path('add-to-favorite/', views.AddToFavorite.as_view(), name="add-to-favorite"),
+    path('favorite-list/', views.FavoriteList.as_view(), name="favorite-list"),
+    path('favorite-delete/<int:pk>/',
+         views.DeleteFavoriteView.as_view(), name="favorite-delete"),
 ]
